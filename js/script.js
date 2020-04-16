a6899b1ee307953a9da227d4ee948abeb2b64bbb
Olga <olga.dovzhyk@gmail.com>
2020-04-16 22:30:49 +0300

    Add Menu Background, Burger Menu, Media.css for header

--- js/script.js
+++ js/script.js
@@ -1,3 +1,56 @@
+/* ............... Header Toggle Cover ................. */
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll == 0) {
+            $(".wrapper").css({
+                "backgroundColor": "transparent"
+            });
+        } else {
+            $(".wrapper").css({
+                "backgroundColor": "rgba(185, 169, 92, 0.8"
+            });
+        }
+    });
+});
+
+/* ............... // Header Toggle Cover .............. */
+
+/* ................. Button Scroll ..................... */
+
+$("a[href='#bottom']").click(function () {
+    $("html, body").animate({
+        scrollTop: $(document).height()
+    }, 2000);
+});
+
+$(".sbtn").click(function () {
+    $("body, html").animate({
+        scrollTop: 2015
+    }, 800);
+});
+
+$("a[href='#toTop']").click(function () {
+    $("html, body").animate({
+        scrollTop: 0
+    }, 2000);
+});
+
+/* .............. .. Button Scroll ..................... */
+
+/* ................. Menu Toggle .................... */
+
+$(document).ready(function () {
+    $("#burger").click(function () {
+        $("nav, .menu, .menuCover, .firstLine, .secondLine, .thirdLine").toggleClass('active');
+    });
+});
+
+/* .............. // Menu Toggle ................. */
+
+
+
 var modal = document.getElementById("myModal");
 
 var btn = document.getElementById("myBtn");
@@ -20,22 +73,22 @@ window.onclick = function (event) {
 
 // Button "TOP"
 
-window.onload = function () {
-    var scrolled;
-    var timer;
-    document.getElementById('toTop').onclick = function () {
-        scrolled = window.pageYOffset;
-        scrollToTop();
-    }
-
-    function scrollToTop() {
-        if (scroll > 0) {
-            window.scrollTo(0, scrolled);
-            scrolled = scrolled - 50; //100- швидкість прокрутки
-            timer = setTimeout(scrollToTop, 100);
-        } else {
-            clearTimeout(timer);
-            window.scrollTo(0, 0);
-        }
-    }
-}
+//window.onload = function () {
+//    var scrolled;
+//    var timer;
+//    document.getElementById('toTop').onclick = function () {
+//        scrolled = window.pageYOffset;
+//        scrollToTop();
+//    }
+//
+//    function scrollToTop() {
+//        if (scroll > 0) {
+//            window.scrollTo(0, scrolled);
+//            scrolled = scrolled - 50; //100- швидкість прокрутки
+//            timer = setTimeout(scrollToTop, 100);
+//        } else {
+//            clearTimeout(timer);
+//            window.scrollTo(0, 0);
+//        }
+//    }
+//}
