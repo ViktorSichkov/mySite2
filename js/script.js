a061dc97acf5d87ff6e702b92f5241f49df913df
Olga <olga.dovzhyk@gmail.com>
2020-04-24 13:00:38 +0300

    Fix button'Book Now', button 'toTop', footer icons, add input-calendar

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 /* ............... Header Toggle Cover ................. */
 
 $(document).ready(function () {
-    $(window).scroll(function () {
-        var scroll = $(window).scrollTop();
-        if (scroll == 0) {
-            $(".wrapper").css({
-                "backgroundColor": "transparent"
-            });
-        } else {
-            $(".wrapper").css({
-                "backgroundColor": "rgba(185, 169, 92, 0.8"
-            });
-        }
-    });
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll == 0) {
+      $(".wrapper").css({
+        backgroundColor: "transparent",
+      });
+    } else {
+      $(".wrapper").css({
+        backgroundColor: "rgba(185, 169, 92, 0.8",
+      });
+    }
+  });
 });
 
 /* ............... // Header Toggle Cover .............. */
@@ -20,21 +20,39 @@ $(document).ready(function () {
 /* ................. Button Scroll ..................... */
 
 $("a[href='#bottom']").click(function () {
-    $("html, body").animate({
-        scrollTop: $(document).height()
-    }, 2000);
+  $("html, body").animate(
+    {
+      scrollTop: $(document).height(),
+    },
+    2000
+  );
 });
 
 $(".sbtn").click(function () {
-    $("body, html").animate({
-        scrollTop: 2015
-    }, 800);
+  $("body, html").animate(
+    {
+      scrollTop: 2015,
+    },
+    800
+  );
 });
 
-$("a[href='#toTop']").click(function () {
-    $("html, body").animate({
-        scrollTop: 0
-    }, 2000);
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 655) {
+      $("#toTop").fadeIn();
+    } else {
+      $("#toTop").fadeOut();
+    }
+  });
+  $("a[href='#toTop']").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      2000
+    );
+  });
 });
 
 /* .............. .. Button Scroll ..................... */
@@ -42,14 +60,14 @@ $("a[href='#toTop']").click(function () {
 /* ................. Menu Toggle .................... */
 
 $(document).ready(function () {
-    $("#burger").click(function () {
-        $("nav, .menu, .menuCover, .firstLine, .secondLine, .thirdLine").toggleClass('active');
-    });
+  $("#burger").click(function () {
+    $(
+      "nav, .menu, .menuCover, .firstLine, .secondLine, .thirdLine"
+    ).toggleClass("active");
+  });
 });
 
 /* .............. // Menu Toggle ................. */
-
-
 
 var modal = document.getElementById("myModal");
 
@@ -58,18 +76,36 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function () {
-    modal.style.display = "block";
-}
+  modal.style.display = "block";
+};
 
 span.onclick = function () {
-    modal.style.display = "none";
-}
+  modal.style.display = "none";
+};
 
 window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+/*  ................................... Calendar .....................................*/
+
+$(document).ready(function () {
+  $("#datepicker")
+    .datepicker({
+      numberOfMonth: 1,
+      changeYear: true,
+      changeMonth: true,
+      showWeek: true,
+      showOtherMonths: true,
+    })
+    .css({
+      color: "green",
+    });
+});
+
+/*  ................................ // Calendar .....................................*/
 
 // Button "TOP"
 
